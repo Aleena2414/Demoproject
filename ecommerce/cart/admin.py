646484c6077c888d36daecade370f344d65be4e7
Aleena2414 <aleenamolps1462003@gmail.com>
2024-10-31 10:50:01 +0530

    added1

--- ecommerce/cart/admin.py
+++ ecommerce/cart/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from cart.models import Cart
 
 from cart.models import Order_details,payment
-
+from http import HttpResponse
 admin.site.register(Cart)
 admin.site.register(Order_details)
 admin.site.register(payment)
